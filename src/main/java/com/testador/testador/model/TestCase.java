b97d54feb86583686af42e547ee3dab8d0670c27
andherreraML <andrea.herrera@mercadolivre.com>
2022-07-26 12:22:07 -0400

    feat: cria entidade TestCase

---
+++ src/main/java/com/testador/testador/model/TestCase.java
@@ -0,0 +1,29 @@
+package com.testador.testador.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter @Setter
+public class TestCase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private boolean tested;
+
+    @Column(nullable = false)
+    private boolean passed;
+
+    @Column(nullable = false)
+    private int numberOfTries;
+
+    private Date lastUpdate;
+}
