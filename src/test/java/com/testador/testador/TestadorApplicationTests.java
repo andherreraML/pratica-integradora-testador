0f93d235b2d1f48b4795a189507823fdbf4f0831
andherreraML <andrea.herrera@mercadolivre.com>
2022-07-26 12:08:31 -0400

    feat: primeiro commit

---
+++ src/test/java/com/testador/testador/TestadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testador.testador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestadorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
