eff46d454837311c951c1dfac5ebd3f6d8529dbe
andherreraML <andrea.herrera@mercadolivre.com>
2022-07-28 16:22:25 -0400

    update: muda tipo Date para LocalDate no Repository

--- src/main/java/com/testador/testador/repository/TestCaseRepo.java
+++ src/main/java/com/testador/testador/repository/TestCaseRepo.java
@@ -3,9 +3,10 @@ package com.testador.testador.repository;
 import com.testador.testador.model.TestCase;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
 public interface TestCaseRepo extends CrudRepository<TestCase, Long> {
-    List<TestCase> findByLastUpdateAfter(Date date);
+    List<TestCase> findByLastUpdateAfter(LocalDate date);
 }
